6fa9959581a5ec0956c18f6b6dbd78edede3e353
LkM <liew@technation.cc>
2013-06-15 12:02:04 +0800

    Commit by LkM on 06/15/2013 Sat 12:01 PM

--- init_autoloader.php
+++ init_autoloader.php
@@ -12,12 +12,35 @@ define('KERNEL_PATH', CORE_PATH . '/kernel');
 if (file_exists('vendor/autoload.php')) {
 	$loader = include 'vendor/autoload.php';
 }
-// Get application stack configuration
-$configuration = include KERNEL_PATH . '/Config/KernelNamespace.php';
+
+$vendorPath = false;
+if (is_dir('vendor/Techfever')) {
+	$vendorPath = 'vendor/Techfever';
+}
+$kernelPath = false;
+if (is_dir('kernel')) {
+	$kernelPath = 'kernel';
+}
+
 if (isset($loader)) {
-	foreach ($configuration['Namespace'] as $name => $path) {
-		$loader->add($name, dirname($path));
+	if ($vendorPath) {
+		$loader->add('Techfever', $vendorPath);
 	}
-	$loader->register();
+	if ($kernelPath) {
+		$loader->add('Kernel', $kernelPath);
+	}
+} else {
+	if ($vendorPath) {
+		include $vendorPath . '/Techfever/Loader/AutoloaderFactory.php';
+		Techfever\Loader\AutoloaderFactory::factory(array(
+			'Zend\Loader\StandardAutoloader' => array(
+				'autoregister_zf' => true
+			)
+		));
+	}
+}
+
+if (!class_exists('Techfever\Loader\AutoloaderFactory')) {
+	throw new RuntimeException('Unable to load TF2. Run `php composer.phar install` or define a TF2_PATH environment variable.');
 }
 ?>
