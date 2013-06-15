2c7ec08ac91d04bfba75e4dfc49633c143d6e3dd
LkM <liew@technation.cc>
2013-06-15 17:44:45 +0800

    Commit by LkM on 06/15/2013 Sat 05:44 PM

--- config/database.config.php
+++ config/database.config.php
@@ -1,6 +1,4 @@
 <?php
 return array(
-	"Database" => array(
-		"driver" => "Mysqli", "host" => "localhost", "port" => "3306", "username" => "root", "password" => "t3chn4t10n", "database" => "technation", "socket" => null
-	)
+	"driver" => "Mysqli", "host" => "localhost", "port" => "3306", "username" => "root", "password" => "t3chn4t10n", "database" => "technation", "socket" => null
 );
--- kernel/Startup.php
+++ kernel/Startup.php
@@ -7,13 +7,13 @@ class Startup {
 	 *
 	 * @var Config
 	 */
-	public static $Config = array();
+	private static $Config = array();
 
 	/**
 	 *
 	 * @var Service
 	 */
-	public static $Service = array();
+	private static $Service = array();
 
 	public static $Instance = null;
 
@@ -32,12 +32,9 @@ class Startup {
 	 */
 	public static function initialize() {
 		// Initialize Config
-		self::$Config = new Config();
-		self::$Config->initialize();
-
+		self::initConfig();
 		// Initialize Service
-		self::$Service = new Service(self::$Config->getConfig());
-		self::$Service->initialize();
+		self::initService();
 	}
 
 	/**
@@ -47,11 +44,9 @@ class Startup {
 	 */
 	public static function uninitialize() {
 		// UnInitialize Config
-		self::$Config->uninitialize();
 		self::$Config = null;
 
 		// UnInitialize Service
-		self::$Service->uninitialize();
 		self::$Service = null;
 
 		self::$Instance = null;
@@ -76,5 +71,82 @@ class Startup {
 	 */
 	public static function render() {
 
+	}
+
+	/**
+	 * Initialize Config
+	 *
+	 * @return void
+	 */
+	public static function initConfig() {
+		$_configPath = CONFIG_PATH . '/';
+		if (file_exists($_configPath)) {
+			$_configFileRaw = scandir($_configPath);
+			foreach ($_configFileRaw as $_configFile) {
+				$_configFilePath = $_configPath . $_configFile;
+				$_configFileInfo = pathinfo($_configFilePath);
+				if (file_exists($_configFilePath) && substr($_configFileInfo['basename'], -10) == 'config.php') {
+					$_configFileInfo['filename'] = substr($_configFileInfo['basename'], 0, -11);
+					self::$Config[strtolower($_configFileInfo['filename'])] = require_once $_configFilePath;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Get Config
+	 *
+	 * @return Array String
+	 */
+	public static function getConfig($name = null, $key = null) {
+		if (!empty($name)) {
+			$name = strtolower($name);
+			if (!empty($key)) {
+				return self::$Config[$name][$key];
+			} else if (array_key_exists($name, self::$Config)) {
+				return self::$Config[$name];
+			}
+		}
+		return self::$Config;
+	}
+
+	/**
+	 * Initialize Service
+	 *
+	 * @return void
+	 */
+	public static function initService() {
+		$_config = self::getConfig();
+		$_servicePath = KERNEL_PATH . '/Service/';
+		if (file_exists($_servicePath)) {
+			$_serviceFileRaw = scandir($_servicePath);
+			foreach ($_serviceFileRaw as $_serviceFile) {
+				$_serviceFilePath = $_servicePath . $_serviceFile;
+				$_serviceFileInfo = pathinfo($_serviceFilePath);
+				if (file_exists($_serviceFilePath) && $_serviceFileInfo['extension'] == 'php' && $_serviceFileInfo['filename'] != 'ServiceClass') {
+					$classname = __NAMESPACE__ . "\Service\\" . $_serviceFileInfo['filename'];
+					$service = strtolower($_serviceFileInfo['filename']);
+					$_config = (array_key_exists($service, $_config) ? $_config[$service] : null);
+					self::$Service[$_serviceFileInfo['filename']]['object'] = new $classname($_config);
+					self::$Service[$_serviceFileInfo['filename']]['object']->start();
+				}
+			}
+		}
+	}
+
+	/**
+	 * Get Service
+	 *
+	 * @return Array String
+	 */
+	public static function getService($name = null, $option = null) {
+		if (!empty($name)) {
+			if (!empty($option)) {
+				return self::$Service[$name][$option];
+			} else if (array_key_exists($name, self::$Service)) {
+				return self::$Service[$name];
+			}
+		}
+		return self::$Service;
 	}
 }
